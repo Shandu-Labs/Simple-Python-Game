4b09a42bf6ddfab69951cac8568c08934429ea80
Shandu-Labs <268568481+Shandu-Labs@users.noreply.github.com>
2026-05-13 14:19:25 +0200

    Add files via upload

---
+++ gamble_Game.py
@@ -0,0 +1,61 @@
+import random
+
+
+def roll():
+    min_value = 1
+    max_value = 6
+    roll = random.randint(min_value, max_value)
+
+    return roll
+
+
+while True:
+    players = input("Enter the number of players (1-4): ")
+
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 4:
+            break
+        else:
+            print("Must be between (2 - 4)")
+
+    else:
+        print("Invalid! Try again.")
+
+max_score = 50
+player_score = [0 for _ in range(players)]
+
+while max(player_score) < max_score:
+
+    for player_index in range(players):
+        print("\nPlayer number", player_index + 1, "turn has just started!\n")
+        print("Your total score is", player_score[player_index], "\n")
+        current_score = 0
+
+        while True:
+            should_roll = input("Would you like to roll again (y)? ")
+
+            if should_roll.lower() != "y":
+                break
+
+            value = roll()
+
+            if value == 1:
+                print("You rolled a 1! Turn is done ")
+                current_score = 0
+                break
+
+            else:
+                current_score += value
+                print("You rolled a:", value)
+
+            print("Your score is:", current_score)
+
+        player_score[player_index] += current_score
+        print("Your total score is:", player_score[player_index])
+
+max_score = max(player_score)
+winning_idk = player_score.index(max_score)
+
+print("Player number", winning_idk + 1,
+      "is the winner with a score of:", max_score)
